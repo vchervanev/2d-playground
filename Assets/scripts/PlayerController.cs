fb071189c7b092e6f7603b00917c7040a1a9a837
Vladimir Chervanev <vchervanev.dev@gmail.com>
2020-11-01 19:05:44 -0600

    bullets prototype [wip]

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    [SerializeField] private GameObject bulletPrefab;
+    private Vector3 lastPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        lastPosition = transform.position - Vector3.up;
     }
 
     // Update is called once per frame
@@ -19,5 +21,23 @@ public class PlayerController : MonoBehaviour
         float dy = Input.GetAxis("Vertical");
         float dx = Input.GetAxis("Horizontal");
         transform.position += new Vector3(dx, dy, 0) * Time.deltaTime * speed;
+
+        if (Input.GetButtonDown("Fire1")) {
+            Vector3 direction = (transform.position - lastPosition);
+            Debug.Log("Position " + transform.position.ToString("F4"));
+            Debug.Log("LastPosition " + lastPosition.ToString("F4"));
+            Debug.Log(direction.ToString("F4"));
+            direction.Normalize();
+            Debug.Log(direction.ToString("F4"));
+
+            Vector3 start = transform.position + 5 * direction;
+            Debug.Log("start " + start.ToString("F4"));
+            GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+            bullet.transform.position = start;
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * 0.5f;
+        }
+        if (Vector3.Distance(lastPosition, transform.position) > 0.1) {
+            lastPosition = transform.position;
+        }
     }
 }
